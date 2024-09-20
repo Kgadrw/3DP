d033bbf9f0fbdef3491249f8da0afe350a91eec5
Bertin M <bertin.m2001@gmail.com>
2024-09-20 16:14:12 +0200

    refined changes

--- src/components/DataAssessment.tsx
+++ src/components/DataAssessment.tsx
@@ -16,7 +16,7 @@ const DataAssessment: React.FC = () => {
       <section className="grid w-11/12 gap-3 p-6 mx-auto md:grid-cols-4">
         <aside className="h-64 p-1 md:h-auto">
           <section className="bg-white relative flex flex-col gap-y-4 md:my-64 h-full md:h-[40%] rounded-lg shadow-md">
-            <h1 className="m-4 text-2xl font-bold">Data Reactive</h1>
+            <h1 className="m-4 text-2xl font-bold text-blue-800">Data Reactive</h1>
             <div className="h-2 p-[4px] w-6/12 self-end bg-gradient-to-tr from-[#fdb740] to-[#fdb740]/80"></div>
             <p className="m-4 text-[#57647c] font-serif">
               Limited use of data, primarily reacting to immediate needs or issues.
@@ -32,7 +32,7 @@ const DataAssessment: React.FC = () => {
         </aside>
         <aside className="h-64 p-1 md:h-auto">
           <section className="bg-white relative flex flex-col gap-y-4 md:my-48 rounded-lg shadow-md h-full md:h-[40%]">
-            <h1 className="m-4 text-2xl font-bold">Data Literate</h1>
+            <h1 className="m-4 text-2xl font-bold text-blue-800">Data Literate</h1>
             <div className="h-2 p-[4px] w-6/12 self-end bg-[#fdb740]"></div>
             <p className="m-4 text-[#57647c] font-serif">
               Foundational understanding of data's value, with employees equipped with basic data skills.
@@ -50,7 +50,7 @@ const DataAssessment: React.FC = () => {
         </aside>
         <aside className="h-64 p-1 md:h-auto">
           <section className="bg-white relative flex flex-col gap-y-4 md:my-24 rounded-lg shadow-md h-full md:h-[40%]">
-            <h1 className="m-4 text-2xl font-bold">Data Ready</h1>
+            <h1 className="m-4 text-2xl font-bold text-blue-800">Data Ready</h1>
             <div className="h-2 p-[4px] w-6/12 self-end bg-gradient-to-tr from-[#fdb740] to-[#fdb740]"></div>
             <p className="m-4 text-[#57647c] font-serif">
               Established data infrastructure and governance, enabling systematic data utilization for decision-making.
@@ -68,7 +68,7 @@ const DataAssessment: React.FC = () => {
         </aside>
         <aside className="h-64 p-1 md:h-auto">
           <section className="bg-white relative flex flex-col gap-y-4 rounded-lg shadow-md h-full md:h-[40%]">
-            <h1 className="m-4 text-2xl font-bold">Data Experts</h1>
+            <h1 className="m-4 text-2xl font-bold text-blue-800">Data Experts</h1>
             <div className="h-2 p-[4px] w-6/12 self-end bg-gradient-to-tr from-[#fdb740] to-[#fdb740]"></div>
             <p className="m-4 text-[#57647c] font-serif">
               Advanced expertise in data analytics and utilization, driving innovation and competitive advantage.
